3f67405ed445ced4336e13605804b8f8b1998ce1
AbhijithNayak <28thstar@gmail.com>
2022-06-21 17:00:24 +0000

    pull itrans mapping from github and print out relevant glyphs

--- src/app.py
+++ src/app.py
@@ -1,8 +1,19 @@
-import numpy as np
-import pandas as pd
 import requests
+import csv
 
-if __name__ == "__main__":
-    r = requests.get("https://raw.github.com/avinash311/itrans/blob/master/js/data/DEFAULT.tsv")
-    print(r.headers['Content-Type'])
-    print(r.text)
+r = requests.get("https://raw.githubusercontent.com/avinash311/itrans/master/js/data/DEFAULT.tsv")
+print(r.headers['Content-Type'])
+with open('full_itrans_map.tsv', 'w') as f:
+    f.write(r.text)
+
+with open('full_itrans_map.tsv', 'rt', errors='ignore', newline='') as f:
+    table = csv.DictReader(f, delimiter='\t')
+    for row in table:
+        ka_info = (row['INPUT'], row['CODE-NAME'], row['#kannada'])
+        if all(ka_info):
+            try:
+                ascii_letter, code_name, ka_letter = (row['INPUT'], row['CODE-NAME'], chr(int(row['#kannada'].rsplit('+')[-1], base=16)))
+            except ValueError:
+                continue
+            else:
+                print(ascii_letter, ka_letter, sep='\t')
